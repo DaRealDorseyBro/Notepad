9992dc8b19f91cd736643744981af315968d56f1
DaRealDorseyBro <65787960+DaRealDorseyBro@users.noreply.github.com>
2020-12-26 11:55:33 -0800

    Thanks for the new remind command cinnamon!

--- commands/notes/remind.js
+++ commands/notes/remind.js
@@ -1,44 +1,59 @@
-const {MessageEmbed} = require('discord.js')
-let pretty = require('pretty-ms')
-const dhms = require('dhms')
+let { MessageEmbed } = require("discord.js"),
+    pretty = require("pretty-ms"),
+    dhms = require("dhms");
 module.exports = {
     name: "remind",
     description: "Remind you to check a note!",
-    type: 'notes',
+    type: "notes",
     cooldown: 5,
-    usage: '< name > < time >',
+    usage: "< name > < time >",
     async execute(client, message, args, db) {
-        if (!args[0]) return message.channel.send('Please add the name of the note!')
-        if (!args[1]) return message.channel.send('Please add when you want to be reminded (Ex: `3d`)!')
+        if (!args[0] || !args[1])
+            return message.channel.send(
+                "Either the value of the reminder or the time for the reminder was not found or found invalid to format time make sure you do something like 3d"
+            );
+        let name = args[0].toLowerCase(),
+            time = dhms(
+                args
+                    .slice(1)
+                    .join(" ")
+                    .toLowerCase()
+            ),
+            dball = db.fetchEverything(),
+            coowner = false,
+            noteowner;
+        if (time > 2073600000 || time <= 0)
+            return message.channel.send(
+                "Please use a valid time which is less then 24 days"
+            );
 
-        let name = args[0].toLowerCase()
-        let time = dhms(args.slice(1).join(' ').toLowerCase())
-
-        if (time > 2073600000) return message.channel.send('Please use a time that\'s under `24` days!')
-        if (time <= 0) return message.channel.send('Please use a valid time!')
-
-        let reminders = 0
+        let reminders = dball.filter(
+            t => t.type === "reminder" && t.owner === message.author.id
+        );
+        if (reminders.length >= 10)
+            return message.channel.send("You already have 10 reminders!");
         await db.fetchEverything().forEach(c => {
-            if (c.type === 'reminder' && c.owner === message.author.id) {
-                reminders += 1
+            if (
+                c.type === "note" &&
+                c.coowner === message.author.id &&
+                c.name === name
+            ) {
+                coowner = true;
+                noteowner = c.owner;
             }
-        })
-        if (reminders > 10) return message.channel.send('You already have 10 reminders!')
-
-        let coowner = false
-        let noteowner;
-        await db.fetchEverything().forEach(c => {
-            if (c.type === 'note' && c.coowner === message.author.id && c.name === name) {
-                coowner = true
-                noteowner = c.owner
-            }
-        })
-        if (!await db.get(`${message.author.id}_${name}`) && coowner === false) return message.channel.send('You don\'t own/co-own a note with that name!')
-        if (await db.get(`remind_${message.author.id}_${name}`)) return message.channel.send('You already have a reminder set for that name!')
-        if (coowner === false) noteowner = message.author.id
+        });
+        if (!(await db.get(`${message.author.id}_${name}`)) && coowner === false)
+            return message.channel.send(
+                "You don't own/co-own a note with that name!"
+            );
+        if (await db.get(`remind_${message.author.id}_${name}`))
+            return message.channel.send(
+                "You already have a reminder set for that name!"
+            );
+        if (coowner === false) noteowner = message.author.id;
 
         await db.set(`remind_${message.author.id}_${name}`, {
-            type: 'reminder',
+            type: "reminder",
             trueOwner: noteowner,
             channel: message.channel.id,
             owner: message.author.id,
@@ -46,13 +61,14 @@ module.exports = {
             time: time,
             now: Date.now(),
             times: 1
-        })
-        return message.channel.send(new MessageEmbed()
-            .setTitle('Reminder: `' + name + '`')
-            .setDescription(`I will remind you in \`\`\`\n${pretty(time)}\`\`\``)
-            .setColor(client.bot.color)
-            .setTimestamp(Date.now() + time)
-            .setFooter(client.bot.footer)
+        });
+        return message.channel.send(
+            new MessageEmbed()
+                .setTitle("Reminder: `" + name + "`")
+                .setDescription(`I will remind you in \`\`\`\n${pretty(time)}\`\`\``)
+                .setColor(client.bot.color)
+                .setTimestamp(Date.now() + time)
+                .setFooter(client.bot.footer)
         );
     }
-}
+};
